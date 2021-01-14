476f7f5370d81385c8b557a0827441f283baadc6
VIBHAV <vibhav@dhanwise.com>
2021-01-14 21:16:03 +0530

    cors error fix

--- app.js
+++ app.js
@@ -24,6 +24,14 @@ app.enable('strict routing');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//this resolve cors error
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next();
+})
+
 // Compression
 app.use(require('compression')({threshold: 2048}));
 
